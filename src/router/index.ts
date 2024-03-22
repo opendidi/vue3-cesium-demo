5196455e6a9025ffdbe109c07edca41b377416e8
lianghuitang@heneng.com <511810970@qq.com>
2024-03-22 15:17:56 +0800

    Merge branch 'master' of https://github.com/xiaodidi-js/vue3-cesium-demo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,3 @@
-/*
- * @Descripttion:
- * @version: 1.0.0
- * @Author: htang
- * @Date: 2024-03-22 09:35:48
- * @LastEditors: htang
- * @LastEditTime: 2024-03-22 11:17:39
- */
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/index.vue'
 import CesiumView from '../views/CesiumView.vue'
@@ -22,6 +14,14 @@ const router = createRouter({
       path: '/',
       name: 'cesium',
       component: CesiumView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -4,50 +4,32 @@
  * @Author: htang
  * @Date: 2024-03-22 09:35:48
  * @LastEditors: htang
- * @LastEditTime: 2024-03-22 10:46:06
+ * @LastEditTime: 2024-03-22 10:09:06
  */
 import { fileURLToPath, URL } from 'node:url'
 
-import { UserConfig, ConfigEnv, loadEnv, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'
-import { wrapperEnv } from './build/utils';
 
 // https://vitejs.dev/config/
-export default ({ command, mode }: ConfigEnv): UserConfig => {
-
-  const isBuild = command === 'build';
-
-  const root = process.cwd();
-
-  const env = loadEnv(mode, root);
-
-  const viteEnv = wrapperEnv(env);
-
-  const { VITE_PUBLIC_PATH } = viteEnv;
-
-  return {
-    base: VITE_PUBLIC_PATH,
-    plugins: [
-      vue(),
-      cesium(),
-    ],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // 这里的内容将会在每个 .scss 文件的开头自动注入
-          additionalData: `$injectedColor: orange;`
-        },
+export default defineConfig({
+  base: 'vue3-cesium-demo',
+  plugins: [
+    vue(),
+    cesium(),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 这里的内容将会在每个 .scss 文件的开头自动注入
+        additionalData: `$injectedColor: orange;`
       },
     },
-    esbuild: {
-      //清除全局的console.log和debug
-      drop: isBuild ? ['console', 'debugger'] : [],
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}
+})
